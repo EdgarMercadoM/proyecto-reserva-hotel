f7f0dbdcab26c30be8cd0dbbfd63ccb761bfbec5
Ivan Jose Bernal <ivan.bernal@genesys.cl>
2024-11-26 05:00:01 -0500

    Add

--- db.js
+++ db.js
@@ -3,11 +3,14 @@ const mysql = require('mysql2');
 
 // Se crea la conexión a la base de datos
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'hotel'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'hotel',
+    port: process.env.DB_PORT || 3306
 });
+
+// mysql://root:ZNFkewTmvVyfBPHTVQRKFwYbnNBKMVtQ@autorack.proxy.rlwy.net:15889/railway
 
 // Se conecta a la base de datos
 connection.connect((err) => {
