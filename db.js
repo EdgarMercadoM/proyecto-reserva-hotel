43332402ba3e2573733199451c2d2c055c56fc89
Ivan Jose Bernal <ivan.bernal@genesys.cl>
2024-11-25 19:09:21 -0500

    Subiendo cambios

---
+++ db.js
@@ -0,0 +1,30 @@
+// Se importa el módulo de express y las dependencias necesarias
+const mysql = require('mysql2');
+
+// Se crea la conexión a la base de datos
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'hotel'
+});
+
+// Se conecta a la base de datos
+connection.connect((err) => {
+
+    // Si hay un error, se imprime en consola
+    if (err) {
+
+        // Se imprime el error
+        console.error('Error conectando a la base de datos:', err.stack);
+
+        // Se detiene
+        return;
+    };
+
+    // Se imprime el id de la conexión
+    console.log('Conectado a la base de datos como id ' + connection.threadId);
+});
+
+// Se exporta la conexión
+module.exports = connection;
